321d962c90d7da3a6fc89a20fbe66c80c7c8b0ca
santicano <canosantiago404@gmail.com>
2024-01-21 12:23:45 -0300

    added users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+const JWT = require('../services/uploadImage');
+router.user(JWT.verify);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:id/suggested_friends', UserController.get_suggested_friends);
+
+router.get('/:id', UserController.get_user);
+
+router.get('/:id/friends', UserController.get_friends);
+
+router.get('/:id/friendrequests', UserController.create_friend_request);
+
+router.post('/:id/profilepic', UserController.update_pic);
+
+router.post('/:id/friendrequests/:requestid/accept', UserController.accept_friend_request);
+
+router.post('/:id/friendrequests/:requestid/decline', UserController.decline_friend_request);
+
+router.post('/:id/bio', UserController.update_bio);
 
 module.exports = router;
